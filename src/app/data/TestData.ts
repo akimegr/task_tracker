97c38066e7e94254114655dbfef1d2a0d8a65069
akimegr <82463845+akimegr@users.noreply.github.com>
2023-01-29 13:48:20 +0300

    git added tasks

---
+++ src/app/data/TestData.ts
@@ -0,0 +1,49 @@
+import {Category} from "../model/Category";
+import {Priority} from "../model/Priority";
+import {Task} from "../model/Task";
+
+
+export class TestData {
+  static categories: Category[] = [
+    {id:1, title:'Work'},
+    {id:2, title:'Family'},
+    {id:3, title:'Study'},
+    {id:4, title:'Relax'},
+    {id:5, title:'Sport'},
+    {id:6, title:'Eat'}
+  ]
+
+  static priority: Priority[] = [
+    {id:1, title: 'Low', color: '#e5e5e5'},
+    {id:1, title: 'Middle', color: '#85D1b2'},
+    {id:1, title: 'Hight', color: '#F1822D'},
+    {id:1, title: 'Very hight', color: '#F1128D'}
+  ]
+
+  static tasks: Task[] = [
+    {
+      id: 1,
+      title: 'Залить бензин',
+      priority: TestData.priority[2],
+      completed: false,
+      category: TestData.categories[9],
+      date: new Date('2019-04-10')
+    },
+    {
+      id: 2,
+      title: 'Залить бензин2',
+      priority: TestData.priority[1],
+      completed: false,
+      category: TestData.categories[4],
+      date: new Date('2019-04-10')
+    },
+    {
+      id: 3,
+      title: 'Залить бензин3',
+      priority: TestData.priority[3],
+      completed: false,
+      category: TestData.categories[4],
+      date: new Date('2019-04-10')
+    }
+  ]
+}
---
+++ src/app/model/Task.ts
@@ -0,0 +1,20 @@
+import {Priority} from "./Priority";
+import {Category} from "./Category";
+
+export class Task {
+  id: number;
+  title: string;
+  priority?: Priority;
+  category?: Category;
+  date?: Date;
+  completed: boolean;
+
+  constructor(id: number,  title: string, completed: boolean, priority?: Priority, category?: Category, date?: Date) {
+    this.id = id;
+    this.title = title;
+    this.priority = priority;
+    this.category = category;
+    this.date = date;
+    this.completed = completed;
+  }
+}
