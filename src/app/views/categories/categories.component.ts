b75b8a436a9f01ec75af372c6faf1b4ff99d45c0
akimegr <82463845+akimegr@users.noreply.github.com>
2023-01-29 14:29:57 +0300

    git added angular materials and bootstrap

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,14 +1,22 @@
 import {Component, OnInit} from '@angular/core';
+import {DataHandlerService} from "../../services/data-handler.service";
+import {Category} from "../../model/Category";
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css']
 })
-export class CategoriesComponent  implements OnInit{
+export class CategoriesComponent implements OnInit {
 
-  constructor() {
+  categories: Category[] | undefined;
+
+  constructor(private dataHandler: DataHandlerService) {
+
   }
+
   ngOnInit() {
+    this.categories = this.dataHandler.getCategories();
+    console.log(this.categories);
   }
 }
